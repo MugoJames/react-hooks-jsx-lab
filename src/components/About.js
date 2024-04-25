ba7df109c94cb14e5d368d7d2a729f2d62570213
James <jamonjoroge816@gmail.com>
2024-04-25 10:14:12 +0300

    hooks JSX

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>Liza has an experience of 10Years and she love's developing new things.</p>
+    <img src={image}alt="I made this" />
+  </div>;
 }
 
 export default About;
